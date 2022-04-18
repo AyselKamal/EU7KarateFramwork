133ff343e4565abb717138f050c8a5d51a69beed
AyselKamal <ayselkamal@gmail.com>
2022-04-19 01:47:52 +0200

    x and y deleted

--- src/test/java/utilities/x.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class x {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
--- src/test/java/utilities/y.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class y {
-    public static void main(String[] args) {
-        System.out.println("World");
-    }
-}
